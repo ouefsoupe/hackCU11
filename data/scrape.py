c27a6ff20416dda635f3d17d29ce2c41b8699f57
standardProton <alexm024680@gmail.com>
2025-03-01 13:46:05 -0700

    Added difficulty to climbs

--- data/scrape.py
+++ data/scrape.py
@@ -5,6 +5,9 @@ import time
 # angle, grade, quality, placements
 
 MAXPAGES = 23
+dropped_count = 0
+total_count = 0
+
 with open("climbs.json", 'w') as f:
     f.write("[\n")
     for offset in range(0, MAXPAGES*1000, 1000):
@@ -18,9 +21,23 @@ with open("climbs.json", 'w') as f:
         print("On page %s/%s" % (int(offset/1000)+1, MAXPAGES))
 
         uuid_list = list_res.json()
+        total_count += len(uuid_list)
         for climb in uuid_list:
+            angle_index = -1
+            for i in range(0, len(climb['climb_stats'])):
+                angle = climb['climb_stats'][i]['angle']
+                if (40 <= angle <= 45):
+                    angle_index = i
+                    break
+            if (angle_index == -1): 
+                dropped_count += 1
+                continue
+
             climb_data = {
+                "difficulty": climb['climb_stats'][angle_index]['difficulty_average'],
                 "placements": climb['placements']
             }
             f.write("    %s,\n" % json.dumps(climb_data))
     f.write("]")
+
+print("Found %s runs, dropped %s" % (total_count, dropped_count))
