8bc5c51d8733f3e2f46acdfffb50eb6467fd17f1
standardProton <alexm024680@gmail.com>
2025-03-01 13:11:03 -0700

    Updated data to 22k climbs

--- data/scrape.py
+++ data/scrape.py
@@ -4,18 +4,18 @@ import time
 
 # angle, grade, quality, placements
 
-MAXPAGES = 150
+MAXPAGES = 23
 with open("climbs.json", 'w') as f:
     f.write("[\n")
     for offset in range(0, MAXPAGES*1000, 1000):
-        time.sleep(0.1)
+        time.sleep(1)
         list_res = req.get("https://drmzubrwofxyzyhicvvx.supabase.co/rest/v1/climbs?select=*&order=total_ascents.desc.nullslast&offset=%s" % offset, 
                         headers={"apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMTI3NDA1MiwiZXhwIjoxOTQ2ODUwMDUyfQ.vBZ8uBgVI3Wc9RaJ2STinaVnd0dY2HHyK42YkqBxUR0",
                                     "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMTI3NDA1MiwiZXhwIjoxOTQ2ODUwMDUyfQ.vBZ8uBgVI3Wc9RaJ2STinaVnd0dY2HHyK42YkqBxUR0"})
         if (list_res.status_code != 200): 
-            print("stopped at i=%s" % (offset/1000))
+            print("Stopped at i=%s" % (offset/1000))
             break
-        print("On page %s/%s" % (int(offset/1000), MAXPAGES))
+        print("On page %s/%s" % (int(offset/1000)+1, MAXPAGES))
 
         uuid_list = list_res.json()
         for climb in uuid_list:
@@ -23,5 +23,4 @@ with open("climbs.json", 'w') as f:
                 "placements": climb['placements']
             }
             f.write("    %s,\n" % json.dumps(climb_data))
-
     f.write("]")
