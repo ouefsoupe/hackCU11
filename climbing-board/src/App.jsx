7a9b03fd9fef3c010459597dcc6898d1ae9d63f8
Aloken Chaudhari <alokench@gmail.com>
2025-03-02 03:07:40 -0700

    button added, slider added, need submit functionalities to properly submit info to flask

--- climbing-board/src/App.jsx
+++ climbing-board/src/App.jsx
@@ -1,4 +1,9 @@
 import { useEffect, useState } from "react";
+import React from 'react'
+import { CRangeSlider } from '@coreui/react-pro'
+import '@coreui/coreui-pro/dist/css/coreui.min.css';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import "./App.css";
 
 const sampleRoute = {
@@ -27,9 +32,9 @@ const SCALE_X = 0.95; // Fine-tune scaling
 const SCALE_Y = 0.95; // Fine-tune scaling
 const CIRCLE_RADIUS = 20; // Hold size
 
-export function Board({size=BOARD_SIZE}) {
+export function Board({route, size=BOARD_SIZE, activeSquares, setActiveSquares}) {
 
-  const [activeSquares, setActiveSquares] = useState({}); // Tracks clicked squares
+  
   const [holds, setHolds] = useState([]); // Holds for overlay circles
   
 
@@ -180,16 +185,57 @@ export function Board({size=BOARD_SIZE}) {
     </>
   );
 }
-  
+
+export const submit = (activeSquares) => {
+
+}
+
 
 function App() {
-  const [board, setBoard] = useState(<Board></Board>);
+  const [activeSquares, setActiveSquares] = useState({}); // Tracks clicked squares
+  const [board, setBoard] = useState(
+    <Board 
+      route={sampleRoute}
+      activeSquares={activeSquares}
+      setActiveSquares={activeSquares}
+    ></Board>
+  );
+ 
 
   return (
     <div className="container">
       <h1>KiltaBord</h1>
       <div className="board-container">
         {board}
+      </div>
+      <h4>Select Grade:</h4>
+      <div className="slider-container">
+        <CRangeSlider
+          max={128}
+          labels={[
+            {
+              value: 0,
+              label: 'V0',
+            },
+            {
+              value: 128,
+              label: 'V12',
+            }
+          ]}
+          tooltipsFormat={(value) => `${0.25 * value}`}
+          value={0}
+        />
+      </div>
+      <div className="generate-button">
+        <Stack direction="row" spacing={2}>
+          <Button 
+            variant="contained" 
+            color="success"
+            onClick={submit}
+            >
+            Generate
+          </Button>
+        </Stack>
       </div>
     </div>
   );
